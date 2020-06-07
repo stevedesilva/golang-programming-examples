9b47d7dfecaac7ea81a16f6cd4256325f6bf9e09
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2020-06-07 14:31:53 +0100

    completed exe

---
+++ cmd/channels/oddevenquit/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	odd := make(chan int)
+	eve := make(chan int)
+	quit := make(chan int)
+
+	go send(eve, odd,quit)
+
+	receive(eve,odd,quit)
+	
+	fmt.Printf("exit from main\n")
+}
+
+func receive(eve,odd,quit <-chan int) {
+	for {
+		select {
+		case v:= <- eve : 
+			fmt.Printf("from eve channel:\t %v\n",v)
+		
+		case v:= <- odd : 
+			fmt.Printf("from odd channel:\t %v\n",v)
+		case v:= <- quit : 
+			fmt.Printf("from quit channel:\t %v\n",v)
+			return
+		}
+	}
+}
+
+func send(eve,odd,quit chan<- int) {
+	for i:=0;i <100;i++ {
+		if i % 2 == 0 {
+			eve <- i
+		} else {
+			odd <- i
+		}
+	}
+	close(eve)
+	close(odd)
+	quit <- 0
+}
