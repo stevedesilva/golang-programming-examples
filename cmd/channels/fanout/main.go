a327e34758e0b7a0a620e186cfbec466e6541b3f
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2020-06-11 14:07:07 +0100

    fanout

---
+++ cmd/channels/fanout/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func main() {
+	c1 := make(chan int)
+	c2 := make(chan int)
+
+	go populate(c1)
+
+	go fanOutInWithLimit(c1, c2)
+
+	for v := range c2 {
+		fmt.Println(v)
+	}
+
+	fmt.Println("about to exit")
+
+}
+
+func populate(c chan int) {
+	defer close(c)
+	for i := 0; i < 10; i++ {
+		c <- i
+	}
+}
+
+func fanOutIn(c1 <-chan int, c2 chan<- int) {
+	defer close(c2)
+	wg := sync.WaitGroup{}
+	const maxgoroutines = 10
+
+	for v := range c1 {
+		fmt.Println("fanOutIn>>", v)
+		wg.Add(1)
+
+		// This code would result in timeconsumingWork receiving the last value of v (e.g. 9),
+		// so we must pass in the current value so the func will take a copy of the value
+		// go func(){
+		// 	c2 <- timeconsumingWork(v)
+		// 	wg.Done()
+		// }()
+
+		go func(v int) {
+			c2 <- timeconsumingWork(v)
+			wg.Done()
+		}(v)
+
+	}
+
+	wg.Wait()
+}
+
+func fanOutInWithLimit(c1 <-chan int, c2 chan<- int) {
+	defer close(c2)
+	wg := sync.WaitGroup{}
+
+	const maxgoroutines = 10
+	wg.Add(maxgoroutines)
+
+	for i := 0; i < maxgoroutines; i++ {
+		// launch 10 go routines
+		go func() {
+			// listen on c1 which blocks until a message is received
+			for v := range c1 {
+				// anon func to run the work and put unto a channel
+				func(v int) {
+					c2 <- timeconsumingWork(v)
+
+				}(v)
+
+			}
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+}
+
+func timeconsumingWork(v int) int {
+	time.Sleep(time.Microsecond * time.Duration(rand.Intn(500)))
+	fmt.Println("timeconsumingWork>>", v)
+	return v + rand.Intn(1000)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module goProgrammingLanguage
 
 go 1.13
+
+require github.com/golang/protobuf v1.4.2
